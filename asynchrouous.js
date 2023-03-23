9a5c7c93a635ef70abf0742e6187c6cb4fe1ae20
unknown <gk4636963@gmail.com>
2023-03-23 14:19:47 +0530

    asynchonous javascript file added

---
+++ asynchrouous.js
@@ -0,0 +1,12 @@
+name1="Gurjeet Kaur";
+setTimeout(()=>{console.log(`The name: ${name1}`);},2000);
+function call_name(name1)
+{
+    console.log(`name call in function: ${name1}`);
+}
+call_name(name1);
+arr=[1,2,3,4,5,6,7,8,['grujeet','kaur'],[1,2,3,4,5,6,7,8,90],99,22,33,445,67,1,1,3,7,4,,6,9,1,['navneet','kaur'],77,44,55,2,1,7,4]
+for(i=0;i<arr.length;i++)
+{
+    console.log(arr[i])
+}
