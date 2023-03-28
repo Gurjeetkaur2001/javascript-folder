94691918a33612d3ddde97289431f318d935ae39
unknown <gk4636963@gmail.com>
2023-03-28 15:23:50 +0530

    my javascript file is added

---
+++ trycatch.js
@@ -0,0 +1,28 @@
+// // try{
+// //     console.log(x);
+// // }
+// // catch(error)
+// // {
+// //     console.log(error)
+// // }
+
+// try{
+//     a=[1,2,34,5,6,7]
+//     console.log(a);
+//     console.log(b)
+// }
+// catch(error)
+// {
+//     console.log("There is error which shows "+error)
+// }
+
+
+
+try{
+  
+    console.log("we are inside the try block")
+}
+catch(error)
+{
+    console.log(error)
+}
